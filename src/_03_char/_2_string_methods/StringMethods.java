7dc1bd7e0bb073deb707708a934eb09322bbd92f
championgirl <66965981+championgirl@users.noreply.github.com>
2020-11-12 16:56:39 -0800

    StringsMethods

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,15 +5,17 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-String variable="Joy";
+String variable="Plant";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
 
 System.out.println(variable.charAt(2));
 
 		for(int i = 0; i < variable.length(); i++) {
+		
 			System.out.println(variable.charAt(i));
-	 if(variable.charAt(i)=='J') {
+	         
+			if(variable.charAt(i)=='J') {
 		 System.out.println("J is at index "+i);
 	 }
 		
@@ -25,12 +27,25 @@ System.out.println(variable.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
 
-		
+		           for(int i=0; i< variable.length(); i++) {
+		        	   System.out.println(variable.charAt(i));
+		           }
 		// 5. Pick a char inside your String, and use a loop to determine
+		          
+		           
+		           for(int i=0; i< variable.length();  i++) {
+		        	   if(variable.charAt(i)=='o') {
+		        		   System.out.println("o is at index " + i);
+		        	   }
+		          
+		               System.out.println(variable.charAt + "is at index" + i );
+		           }
 		//    what position/index in the String the char is located.
+		 
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
+		        
 		
 	}
 }
